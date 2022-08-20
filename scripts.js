e27047dc8938e09dfd226c3f749b7751d807cbe8
Nabegh <nabeghmuhra@nais.co.za>
2022-08-20 13:31:27 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+document.getElementById("demo").innerHTML =
+"X = 5, Y = 6, Z = X + Y. therefore The value of z is " + z + ".";  
